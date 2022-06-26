12386577547c1e81445db441d57a273f12023d02
sakshi611 <101569360+sakshi611@users.noreply.github.com>
2022-06-26 14:48:23 -0700

    Add files via upload

---
+++ Assignment_Day19/src/com/Q1/Main.java
@@ -0,0 +1,15 @@
+package com.Q1;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Map<String,Student> hm = new HashMap<String,Student>();
+		hm.put("Telangana", new Student(21, "Brijesh", 450));
+		hm.put("Punjab", new Student(24, "Sonia", 550));
+		hm.put("Uttar Pradesh", new Student(26, "Sakshi", 150));
+		hm.put("Bihar", new Student(27, "Anubhav", 250));
+		hm.put("Karnataka", new Student(29, "Chandani", 350));
+		
+		
+		
+	}
+}
